c345dfba5bdb8f89bc7f1a273092c5e809945012
MrsGillo <amrei.koch@web.de>
2024-07-29 19:11:30 +0200

    Taschenrechner programmiert

--- game.js
+++ game.js
@@ -1,61 +1,39 @@
-// console.log(5===5)
-// console.log(5 != 5)
-// console.log(5 < 4)
-// console.log(5 > 4)
-// console.log(5 >= 5)
-// console.log(5 <= 5)
-// console.log(5===5)
+const themaFunktionen = () => {
 
-let woodInStorage = 10
-const BUILDING_COST = 5
-
-if(woodInStorage >= BUILDING_COST) {
-    console.log("Du baust eine Farmerhütte")
-} else {
-    console.log("Zu wenig Rohstoffe in deinem Kontor")
-}
-
-let costOfFood = 200
-let costOfRent = 1800
-const BUDGET = 2000
-
-if((costOfFood + costOfRent)< BUDGET) {
-    console.log("Du bist im Budget")
-} else if ((costOfFood + costOfRent) === BUDGET) {
-    console.log("Du hast es auf den Punkt getroffen!")
-} else {
-    console.log("Du hast in diesem Monat zu viel ausgegeben!")
-}
-
-let whoOpensTheDoor = "Niffler"
-if(whoOpensTheDoor === "Amy" || whoOpensTheDoor === "Gil") {
-    console.log("Amy oder Gilby kommt nach Hause")
-} else {
-    console.log("Heee..., " + whoOpensTheDoor + " wohnt hier nicht!")
-}
-
-let person01 = "Niffler"
-let person02 = "Joanah"
-
-if(person01 === "Amy" && person02 === "Gil" ) {
-    console.log("Amy und Gil kommen zusammen nach Hause.")
-} else {
-    if(person01 === "Amy" || person02 === "Gil") {
-        console.log("Nur einer von beiden kommt nach Hause.")
-    } else {
-        console.log("Keiner kommt nach Hause, der hier wohnt.")
+    // Code aus der Anleitung
+    const taschenrechner = (firstNumber, secondNumber, operator) => {
+        if(operator === "add") {
+            console.log(firstNumber + secondNumber)
+        } else if(operator === "min") {
+            console.log(firstNumber - secondNumber)
+        } else if(operator === "multi") {
+            console.log(firstNumber*secondNumber)
+        } else if(operator === "div") {
+            console.log(firstNumber/secondNumber)
+        } else{
+            console.log("Gib einen gültigen Operator mit.")
+        }
     }
+    
+    taschenrechner(2, 5, "add")
+    taschenrechner(2, 5, "min")
+    taschenrechner(2, 5, "multi")
+    taschenrechner(2, 5, "div")
+    taschenrechner(2,5)
 }
 
-let highScore = 25
-let score = 26
+themaFunktionen()
 
-if(score > highScore) {
-    console.log("Speichere den neuen Highscore in die Datenbank.")
+const themaArray = () => {
+    // Coder aus der Anleitung
+    console.log("Ich komme aus dem Thema arrays")
+
+
+
+
+
+
+
 }
 
-
-
-
-
-
+themaArray()
