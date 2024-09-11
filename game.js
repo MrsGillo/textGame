d07d5ee5fe5875f4c894bd570a745887b909c9fd
MrsGillo <amrei.koch@web.de>
2024-09-11 15:06:22 +0200

    Grund-Funktionen für das Spiel eingegeben

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const startContainer = document.getElementById("start-container")
     const gameContainer = document.getElementById("game-container")
     const nameField = document.getElementById("name-field")
+    const textElement = document.getElementById('text')
+    const optionButtonsElement = document.getElementById('option-buttons')
+    const imageElement = document.getElementById('image')
 
     // Klick-Handler für Start-Button    
     startButton.addEventListener('click', function() {
@@ -12,6 +15,42 @@ document.addEventListener('DOMContentLoaded', function() {
             startContainer.style.display = "none"
             gameContainer.style.display = "flex"
         }
-        
     })
+
+    // Definition des Szenen-Arrays
+    let scenes = []
+
+    // erste Funktion zum Starten des Spiels
+    const startGame = () => {
+        console.log("Spiel gestartet")
+    }
+
+    // diese Funktion gibt den Inhalt der Szene aus
+    const showSceneContent = (sceneID) => {
+
+    }
+
+    // diese Funktion verarbeitet die Auswahl der nächsten Option
+    const selectOption = (option) => {
+
+    }
+
+    // diese Funktion enthält den Inhalt der Szene
+    const fillSceneData = () => {
+        scenes = [
+            {
+                id: 1,
+                text: "Scene Text 01"
+            }
+        ]
+    }
+
+
+
+
+
+
+
+
+
 })
