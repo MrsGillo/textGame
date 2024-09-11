fa85e79861ff2fbb5e0f0815aab157620cb29304
MrsGillo <amrei.koch@web.de>
2024-09-11 14:46:19 +0200

    Name-Input-Verifizierung

--- game.js
+++ game.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const startButton = document.getElementById("start-button")
     const startContainer = document.getElementById("start-container")
     const gameContainer = document.getElementById("game-container")
+    const nameField = document.getElementById("name-field")
 
     // Klick-Handler für Start-Button    
     startButton.addEventListener('click', function() {
-        startContainer.style.display = "none"
-        gameContainer.style.display = "flex"
+        if(nameField.value === "") {
+            alert("Bitte gib einen Namen ein.")
+        } else {
+            startContainer.style.display = "none"
+            gameContainer.style.display = "flex"
+        }
+        
     })
 })
