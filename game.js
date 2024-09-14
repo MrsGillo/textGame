7257740fb44df3df7267c36b368bf144c5e2f8d5
MrsGillo <amrei.koch@web.de>
2024-09-14 12:51:51 +0200

    dynamischen Szenenwechsel erstellt

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const gameContainer = document.getElementById("game-container")
     const nameField = document.getElementById("name-field")
     const textElement = document.getElementById('text')
-    const optionButtonsElement = document.getElementById('option-buttons')
+    const optionButtonsElement = document.getElementById('options-buttons')
     const imageElement = document.getElementById('image')
     let playerName = ""
 
@@ -37,17 +37,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
         scenes.forEach(scene => {
             if (sceneID === scene.id) {
-                sceneContent = scene
-                console.log(sceneContent)    
+                sceneContent = scene   
             }  
         })
 
         textElement.innerText = sceneContent.sceneDescription
+        imageElement.src = sceneContent.image
+
+        while(optionButtonsElement.firstElementChild) {
+            optionButtonsElement.removeChild(optionButtonsElement.firstElementChild)
+        }
+
+        sceneContent.options.forEach(option => {
+            
+            const newButton = document.createElement('button')
+
+            newButton.innerText = option.optionText
+            newButton.classList.add('btn')
+            newButton.addEventListener('click', () => selectOption(option))
+
+            optionButtonsElement.appendChild(newButton)
+        })
     }
 
     // diese Funktion verarbeitet die Auswahl der nächsten Option
     const selectOption = (option) => {
-
+        showSceneContent(option.nextScene)
     }
 
     // diese Funktion enthält den Inhalt der Szene
@@ -56,6 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
             {
                 id: 1,
                 sceneDescription: "Hoi " + playerName + ", du erwachst in einem verträumten Wald, riechst die Blumen und hörst die Vögel zwitschern.",
+                image: "img/image01.jpg",
                 options: [
                     {
                         optionText: "Du geniesst den Moment, schliesst deine Augen wieder und schlummerst weiter.",
@@ -64,18 +80,21 @@ document.addEventListener('DOMContentLoaded', function() {
                     {
                         optionText: "Du stehst auf, streckst dich einmal durch und gehst Richtung Süden.",
                         nextScene: 2
-                    }
+                    },
                 ]
             },           
             {
                 id: 2,
                 sceneDescription: "Du kommst auf deinem Weg an eine Verzweigung.",
+                image: "img/image02.jpg",
                 options: [
                     {
                         optionText: "Du gehst den linken Pfad entlang.",
+                        nextScene:3
                     },
                     {
-                        optionText: "Du gehst den rechten Pfad entlang."
+                        optionText: "Du gehst den rechten Pfad entlang.",
+                        nextScene: 4
                     }
                 ]
             }
